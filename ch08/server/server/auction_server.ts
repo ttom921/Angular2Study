b738d7a3dee878ceb2e8ef071d6550e1c2ac8267
ttom921 <ttom921@hotmail.com>
2018-01-23 22:10:41 +0800

    第八章

--- ch08/server/server/auction_server.ts
+++ ch08/server/server/auction_server.ts
@@ -19,6 +19,22 @@ const products: Product[] = [
     new Product(5, "第五個商品", 5.99, 2.5, "這是第五商品，是我在學習繤課網Angular入冊實戰", ["機車商品", "硬體設備"]),
     new Product(6, "第六個商品", 6.99, 3.5, "這是第六商品，是我在學習繤課網Angular入冊實戰", ["電子商品", "圖書"]),
 ];
+export class Comment {
+    constructor(public id: number,
+      public procuteId: number,
+      public timesstamp: string,
+      public user: string,
+      public rating: number,
+      public content: string
+    ) {
+    }
+  }
+const comments: Comment[] = [
+    new Comment(1, 1, "2017-02-02 20:20:22", "張三", 3, "東西不錯"),
+    new Comment(2, 1, "2017-03-02 21:21:22", "李四", 3, "東西不錯"),
+    new Comment(3, 1, "2017-04-02 22:22:22", "王五", 3, "東西不錯"),
+    new Comment(4, 2, "2017-05-02 23:23:22", "趙六", 3, "東西不錯")
+  ];
 
 app.get('/', (req, res) => {
     res.send("Hello express");
@@ -30,6 +46,10 @@ app.get('/api/products', (req, res) => {
 });
 app.get('/api/products/:id', (req, res) => {
     res.json(products.find((product) => product.Id == req.params.id));
+
+});
+app.get('/api/products/:id/comments', (req, res) => {
+    res.json(comments.filter((comment:Comment)=> comment.procuteId == req.params.id));
 
 });
 
